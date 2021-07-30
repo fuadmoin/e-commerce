9b3e3ddaa2485bd6fd0ca01859b170aa88105dab
kirubel12 <kirubelhabte1991@gmail.com>
2021-07-30 06:50:09 -0400

    Session added

--- admin/adminCheck.php
+++ admin/adminCheck.php
@@ -1,20 +1,24 @@
 <?php
+session_start();
+if(isset($_POST['login'])){
 include ("../partials/db.php");
-
-
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $set = "SELECT *  FROM admins where username='$email' AND password='$password'";
 
 $result = $conn->query($set);
+
 $final  = $result->fetch_assoc();
+$_SESSION['email'] = $final['username'];
+$_SESSION['password'] = $final['password'];
+
 
 if ($email == $final['username'] AND $password == $final['password']){
     header("Location:adminindex.php");
 }else{
     header("Location: adminLogin.php");
 }
-
+}
 
 ?>
